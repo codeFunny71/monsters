24b921bd12d390bef389dd9f0d382b8d56d6e123
jsuhoversnik <jake.suhoversnik@gmail.com>
2019-02-06 16:09:59 -0500

    added setSmell to swampthing

--- classes/swampthing.php
+++ classes/swampthing.php
@@ -3,16 +3,20 @@ class Swampthing extends Monster
 {
     private static $_smell;
 
-    function __construct($name)
+    function __construct($name, $smell)
     {
         parent::__construct($name);
         parent::setName($name);
 
+        self::$_smell = $smell;
     }
     function attack()
     {
         echo "Gurgle gurgle blub";
-
+    }
+    function setSmell($var)
+    {
+        self::$_smell = $var;
     }
     function getSmell()
     {
